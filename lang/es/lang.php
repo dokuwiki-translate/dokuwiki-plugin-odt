5111715c25df6c7411a41b2679902bba1973a259
Paúl Sanz <polkillas@polkillas.net>
2015-04-27 18:15:24 +0200

    translation update

---
+++ lang/es/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Paúl Sanz <polkillas@polkillas.net>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['view']                  = 'Exportar página al formato Open Document';
+$lang['export_odt_button']     = 'Exportar ODT';
+$lang['tpl_not_found']         = 'AVISO: La plantilla ODT "%s" no se encuentra en el directorio de plantillas "%s". Se usará la plantilla predeterminada.';
---
+++ lang/es/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Paúl Sanz <polkillas@polkillas.net>
+ */
+$lang['tpl_dir']               = 'Subdirectorio para las plantillas en el gestor de medios.';
+$lang['showexportbutton']      = 'Mostrar botón Exportar ODT';
