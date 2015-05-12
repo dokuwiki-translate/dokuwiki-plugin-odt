5627c4efe951f564cae4eda75801a5d7f4f818e4
Per Sjoholm <per.t.sjoholm@flysta.se>
2015-05-12 23:05:09 +0200

    translation update

--- lang/da/lang.php
+++ lang/da/lang.php
@@ -4,9 +4,10 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Søren Birk <soer9648@eucl.dk>
+ * @author Per Sjoholm <per.t.sjoholm@flysta.se>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
-$lang['view']                  = 'Eksportér side til Open Document format';
-$lang['export_odt_button']     = 'ODT-eksport';
-$lang['tpl_not_found']         = 'ADVARSEL : ODT-skabelonen "%s" blev ikke fundet i skabelonmappen "%s". Benytter standardskabelon.';
+$lang['view']                  = 'Exportera sidan till Open Dokument format';
+$lang['export_odt_button']     = 'ODT export';
+$lang['tpl_not_found']         = 'Varning: ODT Mall "%s" hittades inte i mall biblioteket "%s". Använder standard mall.';
--- lang/da/settings.php
+++ lang/da/settings.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Søren Birk <soer9648@eucl.dk>
+ * @author Per Sjoholm <per.t.sjoholm@flysta.se>
  */
-$lang['tpl_dir']               = 'Undermappe for skabelonerne i mediemanageren';
-$lang['showexportbutton']      = 'Vis ODT Eksportknap';
+$lang['tpl_dir']               = 'Under bibliotek för mallar i media hanterare';
+$lang['showexportbutton']      = 'Visa ODT Export Knapp';
